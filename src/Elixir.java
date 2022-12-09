8d1889988201e1e02256d1d20163f0ec3a540aef
Oscar Love <olabradalove@gmail.com>
2022-12-09 17:15:59 +0000

    Done parsing into XML

--- src/Elixir.java
+++ src/Elixir.java
@@ -1,5 +1,8 @@
+import jakarta.xml.bind.annotation.*;
 import java.util.List;
 
+
+@XmlRootElement(name = "elixir")
 public class Elixir {
     public String id;
     public String name;
@@ -8,19 +11,26 @@ public class Elixir {
     public String characteristics;
     public String time;
     public String difficulty;
+
+    @XmlElementWrapper
+    @XmlElement(name = "ingredient")
     public List<Ingredients> ingredients;
+
+    @XmlElementWrapper
+    @XmlElement(name = "inventor")
     public List<Inventor> inventors;
     public String manufacturer;
 
+    @XmlRootElement(name = "ingredient")
     private static class Ingredients {
         public String id;
         public String name;
     }
 
+    @XmlRootElement(name = "inventor")
     private static class Inventor {
         public String id;
         public String firstName;
         public String lastName;
     }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,48 @@
 import com.google.gson.Gson;
-
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.annotation.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
+
+
 
 public class Main {
     private static final String API_URL = "https://wizard-world-api.herokuapp.com/";
 
-    public static void main(String[] args) {
+    @XmlRootElement(name = "elixirs")
+    static class Elixirs {
+        public Elixirs(){
+            elixirsList = new ArrayList<>();
+        }
+
+        private Elixirs(Elixir[] elixirs) {
+            elixirsList = List.of(elixirs);
+        }
+
+        @XmlElement(name = "elixir")
+        List<Elixir> elixirsList;
+    }
+
+    public static void main(String[] args) throws JAXBException {
+        parseAPI();
+    }
+
+    private static void parseAPI() throws JAXBException {
         String jsonTest = readFromUrlStr(API_URL+"Elixirs");
 
         Elixir[] elixirs = new Gson().fromJson(jsonTest, Elixir[].class);
 
-        for (Elixir elixir: elixirs) {
-            System.out.println(elixir.id);
-        }
+        Marshaller marshaller = JAXBContext.newInstance(Elixirs.class).createMarshaller();
+
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        marshaller.marshal(new Elixirs(elixirs), System.out);
     }
 
     private static String readFromUrlStr(String url) {
